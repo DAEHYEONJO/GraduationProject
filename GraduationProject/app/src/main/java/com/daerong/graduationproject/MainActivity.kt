85333e0438b25fe3a733806dd3669e0742d2bcdf
DAEHYEONJO <jdhqpt12@gmail.com>
2021-05-27 15:55:51 +0900

    gradle 수정

--- GraduationProject/app/src/main/java/com/daerong/graduationproject/MainActivity.kt
+++ GraduationProject/app/src/main/java/com/daerong/graduationproject/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.daerong.graduationproject
 
-import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
